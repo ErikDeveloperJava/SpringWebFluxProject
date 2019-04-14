ded3ee81d9aac42b2cf32faa9fb0ca6a99da9f76
ZhamakochyanErik <zhamakochyan.erik@gmail.com>
2019-04-14 23:29:12 +0400

    commit new logic

--- src/main/java/test2/spring/webflux/controller/MovieController.java
+++ src/main/java/test2/spring/webflux/controller/MovieController.java
@@ -1,5 +1,6 @@
 package test2.spring.webflux.controller;
 
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,9 @@ import test2.spring.webflux.model.User;
 import test2.spring.webflux.repository.MovieRepository;
 import test2.spring.webflux.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
@@ -25,21 +29,19 @@ public class MovieController {
     private UserRepository userRepository;
 
     @PostMapping("/{id}/add-user-favourite")
-    public Mono<ResponseEntity> addFavourite(@PathVariable("id") String strId,
+    public Mono addFavourite(@PathVariable("id") String strId,
                                              @AuthenticationPrincipal CurrentUser currentUser) {
+        List<Movie> movies = new ArrayList<>();
         return movieRepository.findById(strId)
-                .defaultIfEmpty(new Movie())
-                .map(movie -> {
-                    if (movie.getId() == null) {
-                        return ResponseEntity
-                                .notFound()
-                                .build();
-                    }
+                .switchIfEmpty(Mono.error(NotFound::new))
+                .filter(movie -> {
+                    movies.add(movie);
                     currentUser.getUser().getMovieList().add(movie);
-                    return userRepository.save(currentUser.getUser())
-                            .map(user -> ResponseEntity.ok(movie))
-                            .block();
-                });
+                    return true;
+                })
+                .then(userRepository
+                        .save(currentUser.getUser())
+                        .map(user -> movies.get(movies.size() - 1)));
     }
 
     @PostMapping
@@ -50,5 +52,12 @@ public class MovieController {
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Movie> getAll(){
         return movieRepository.findAll();
+    }
+
+    @GetMapping("/user/favourite")
+    public Flux<Movie> getAllByUserFavourite(@AuthenticationPrincipal CurrentUser currentUser){
+        return userRepository.findById(currentUser.getUser().getId())
+                .map(User::getMovieList)
+                .flatMapMany(Flux::fromIterable);
     }
 }
