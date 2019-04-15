aca1f453e8f82344b92b8f6dc88bebf8636e4459
ZhamakochyanErik <zhamakochyan.erik@gmail.com>
2019-04-15 17:01:24 +0400

    commit add and delete user's favourite movie logic

--- src/main/java/test2/spring/webflux/controller/MovieController.java
+++ src/main/java/test2/spring/webflux/controller/MovieController.java
@@ -1,6 +1,5 @@
 package test2.spring.webflux.controller;
 
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +7,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 import test2.spring.webflux.config.CurrentUser;
 import test2.spring.webflux.model.Movie;
 import test2.spring.webflux.model.User;
 import test2.spring.webflux.repository.MovieRepository;
 import test2.spring.webflux.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -28,36 +23,73 @@ public class MovieController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/{id}/add-user-favourite")
+    @PostMapping("/{id}/favourite")
     public Mono addFavourite(@PathVariable("id") String strId,
-                                             @AuthenticationPrincipal CurrentUser currentUser) {
-        List<Movie> movies = new ArrayList<>();
+                             @AuthenticationPrincipal CurrentUser currentUser) {
         return movieRepository.findById(strId)
-                .switchIfEmpty(Mono.error(NotFound::new))
-                .filter(movie -> {
-                    movies.add(movie);
-                    currentUser.getUser().getMovieList().add(movie);
-                    return true;
-                })
-                .then(userRepository
-                        .save(currentUser.getUser())
-                        .map(user -> movies.get(movies.size() - 1)));
+                .defaultIfEmpty(Movie
+                        .builder()
+                        .id("empty")
+                        .build())
+                .flatMap(movie -> {
+                    if (movie.getId().equals("empty")) {
+                        return Mono.just(ResponseEntity
+                                .notFound()
+                                .build());
+                    } else {
+                        currentUser.getUser().getMovieList().add(movie);
+                        return userRepository
+                                .save(currentUser.getUser())
+                                .map(user -> ResponseEntity.ok(movie));
+                    }
+                });
     }
 
     @PostMapping
-    public Mono<Movie> add(@RequestBody Movie movie){
+    public Mono<Movie> add(@RequestBody Movie movie) {
         return movieRepository.save(movie);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux<Movie> getAll(){
+    public Flux<Movie> getAll() {
         return movieRepository.findAll();
     }
 
     @GetMapping("/user/favourite")
-    public Flux<Movie> getAllByUserFavourite(@AuthenticationPrincipal CurrentUser currentUser){
+    public Flux<Movie> getAllByUserFavourite(@AuthenticationPrincipal CurrentUser currentUser) {
         return userRepository.findById(currentUser.getUser().getId())
                 .map(User::getMovieList)
                 .flatMapMany(Flux::fromIterable);
+    }
+
+    @DeleteMapping("/{movieId}/favourite")
+    public Mono<ResponseEntity> deleteUserFavouriteMovie(@AuthenticationPrincipal CurrentUser currentUser,
+                                                @PathVariable("movieId") String movieId) {
+        return movieRepository
+                .findById(movieId)
+                .defaultIfEmpty(Movie
+                        .builder()
+                        .id("empty")
+                        .build())
+                .flatMap(movie -> {
+                    if (movie.getId().equals("empty")) {
+                        return Mono.just(ResponseEntity.notFound().build());
+                    } else {
+                        return userRepository
+                                .findById(currentUser.getUser().getId())
+                                .flatMap(user -> {
+                                    if (user.getMovieList().contains(movie)) {
+                                        user.getMovieList().remove(movie);
+                                        return userRepository
+                                                .save(user)
+                                                .flatMap(user1 -> Mono.just(ResponseEntity
+                                                        .ok("movie deleted successfully")));
+                                    } else {
+                                        return Mono.just(ResponseEntity
+                                                .ok("this movie does not exist in the user movie list"));
+                                    }
+                                });
+                    }
+                });
     }
 }
